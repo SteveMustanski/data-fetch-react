d29974bc78cf4778a0b9bfe3fb1b4964a4210a48
Steve Mustanski <steve.mustanski@gmail.com>
2018-12-17 13:19:51 -0500

    added default dogs to search and called default search on componentDidMount

--- src/App.js
+++ src/App.js
@@ -14,18 +14,10 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    axios.get('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
-      .then(response => {
-        this.setState({
-          gifs: response.data.data
-        })
-      })
-      .catch(error => {
-        console.log('Error fetching and parsing data', error);
-      });
+    this.performSearch();
   }
 
-  performSearch = (query) => {
+  performSearch = (query = 'dogs') => {
     axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
     .then(response => {
       this.setState({
